5bab83cde92563565775e77d7bb65b8db97e9fc6
Manuel Stofer <manuelstofer@gmail.com>
2013-02-11 22:26:53 +0100

    Adds support for writing data from Backbone models to the DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var pflock = require('pflock');
+module.exports = adapter;
 
+function adapter (element, model) {
+    'use strict';
+    var api = pflock(element, model.toJSON());
+    
+    return api;
+}
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var myModule = require('backbone-pflock'),
+var adapter = require('backbone-pflock'),
     Backbone = require('solutionio-backbone');
 
 describe('test setup', function () {
@@ -7,3 +7,33 @@ describe('test setup', function () {
     });
 });
 
+
+describe('backbone-pflock', function () {
+
+    var el,
+        model;
+
+    beforeEach(function () {
+        el = $('#backbone-pflock').clone().get(0);
+        $(el).appendTo('body');
+
+        model = new Backbone.Model({
+            username:       'Yohn Yapan',
+            description:    'foo',
+            type:           '1',
+            speed:          '9'
+        });
+    });
+
+    var documentEqualsModel = function () {
+        $(el).find('.username').val().should.equal(model.get('username'));
+        $(el).find('.description').val().should.equal(model.get('description'));
+        $(el).find('.type').val().should.equal(model.get('type'));
+        $(el).find('.speed').val().should.equal(model.get('speed'));
+    };
+
+    it('should write the model data to the document', function () {
+        adapter(el, model);
+        documentEqualsModel();
+    });
+});
