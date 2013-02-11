7f74fd30929486f3538d7e7259fcaf9adabb7879
Manuel Stofer <manuelstofer@gmail.com>
2013-02-11 21:32:48 +0100

    Adds basic test setup

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var myModule = require('backbone-pflock'),
+    Backbone = require('solutionio-backbone');
+
+describe('test setup', function () {
+    it('should work', function () {
+        true.should.not.equal(false);
+    });
+});
+
