134029d745c5d9a65f27a552d23608e76d0302b8
Manuel Stofer <manuelstofer@gmail.com>
2013-02-11 22:52:26 +0100

    Adds support for pushing bound data back to backbone model

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var pflock = require('pflock');
 module.exports = adapter;
 
+/**
+ * Backbone Models Adapter for Pflock
+ * @param element
+ * @param model
+ * @return {Object}
+ */
 function adapter (element, model) {
     'use strict';
-    var api = pflock(element, model.toJSON());
-    
-    return api;
+
+    var binding = pflock(element, model.toJSON());
+
+    binding.on('changed', function (path, value) {
+        var attribute = path.replace(/^\./,'');
+        if (attribute.match(/\./)) {
+            throw new Error('Nested attributes are not supported by the backbone-pflock adapter');
+        }
+        model.set(attribute, value);
+    });
+
+    return binding;
 }
--- test/test.js
+++ test/test.js
@@ -7,6 +7,15 @@ describe('test setup', function () {
     });
 });
 
+var example = new Backbone.Model({
+    username:       'Yohn Yapan',
+    description:    'foo',
+    type:           '1',
+    speed:          '9'
+});
+
+adapter($('#backbone-pflock').get(0), example);
+
 
 describe('backbone-pflock', function () {
 
@@ -25,6 +34,10 @@ describe('backbone-pflock', function () {
         });
     });
 
+    afterEach(function () {
+        $(el).remove();
+    });
+
     var documentEqualsModel = function () {
         $(el).find('.username').val().should.equal(model.get('username'));
         $(el).find('.description').val().should.equal(model.get('description'));
@@ -36,4 +49,26 @@ describe('backbone-pflock', function () {
         adapter(el, model);
         documentEqualsModel();
     });
+
+    it('push changes in the document back to the backbone model', function () {
+        adapter(el, model);
+
+        $(el).find('.username').val('changed');
+        $(el).find('.description').val('bla');
+        $(el).find('.type').val('0');
+        $(el).find('.speed').val('3');
+
+        triggerEvent($(el).find('.username').get(0), 'change');
+        triggerEvent($(el).find('.description').get(0), 'change');
+        triggerEvent($(el).find('.type').get(0), 'change');
+        triggerEvent($(el).find('.speed').get(0), 'change');
+
+        documentEqualsModel();
+    });
 });
+
+function triggerEvent (element, event) {
+    var evt = document.createEvent('Event');
+    evt.initEvent(event, true, true);
+    element.dispatchEvent(evt);
+}
